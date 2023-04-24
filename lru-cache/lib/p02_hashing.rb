192e5c90f00d4c1c89ad2d80e13b7a67d2135832
laurencary <armstronglg720@gmail.com>
2023-04-24 15:31:17 -0700

    finished p02

--- lru-cache/lib/p02_hashing.rb
+++ lru-cache/lib/p02_hashing.rb
@@ -32,6 +32,20 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash = 0
+    self.each do |k,v|
+      hash += convert_to_hash(k) + convert_to_hash(v)
+    end
+
+    hash
+  end
+
+  def convert_to_hash(value)
+    alpha = ("a".."z").to_a
+    if value.is_a?(Integer)
+      return value
+    else 
+      return alpha.index(value.to_s)
+    end
   end
 end
