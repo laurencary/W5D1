d251aa8924b2d68c11e6d1debcf93f9f5134446f
Matthew Montejo <montejododger@yahoo.com>
2023-04-24 15:05:35 -0700

    finished arr & string of P2

--- lru-cache/lib/p02_hashing.rb
+++ lru-cache/lib/p02_hashing.rb
@@ -1,14 +1,30 @@
 class Integer
   # Integer#hash already implemented for you
+
 end
 
 class Array
   def hash
+    uniq = ""
+    self.each_with_index do |el, i|
+      uniq += el.to_s + i.to_s
+    end
+
+    uniq.to_i
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+
+
+    uniq = self.chars.map do |char|
+      alpha.index(char).to_s
+    end
+
+    uniq.join.to_i
+
   end
 end
 
